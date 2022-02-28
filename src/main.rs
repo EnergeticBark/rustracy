c47f0881659700f8828c7dd71e51cebad0762305
EnergeticBark <seth.humphries@live.com>
2022-02-27 20:47:25 -0500

    Colored sphere by normalized vectors

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ mod color;
 mod ray;
 mod vec3;
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc = r.origin() - center;
     let a = vec3::dot(r.direction(), r.direction());
     let b = 2.0 * vec3::dot(r.direction(), oc);
     let c = vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::from(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::from(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::from(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::from(0.0, 0.0, -1.0));
+        return 0.5 * Color::from(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5*(unit_direction.y() + 1.0);
